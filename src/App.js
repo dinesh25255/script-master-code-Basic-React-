60fbef6760945efcda3e59854cffbec944a12d5e
dinesh25255 <dineshbhatt0707@gmail.com>
2024-03-06 01:19:42 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+function App(){
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+    <>
+    <center>
+
+    <h1>
+    <p>Hello ! I am Dinesh Bhatt</p>
+    </h1>
+
+    <p>Today i learned about react. What is react why and when to use react.
+          Setup node and npm. create a react app using npx and npm vite. What is
+          package.json in our react app source code/project root folder and
+          finally edit the src directory.
+          
+          </p>
+
+
+
+
+    
+    </center>
+    <p>Outside center tag</p>
+
+    </>
+
   );
-}
+  
+  }
+
+
+
 
 export default App;
---
+++ src/script.js
@@ -0,0 +1,11 @@
+function script(){
+    return(
+        <center>
+            <h1>from script.js</h1>
+
+        </center>
+
+    );
+}
+
+export default script;
